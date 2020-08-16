5cead6b46089e2f17a9187bdc5a2ad97b010f91f
0oTMNo0 <taban.nbz@gmail.com>
2020-08-16 16:12:11 +0430

    0.1 version colors.xml

--- app/src/main/java/com/example/temusic/Ui/Activitys/MainActivity.kt
+++ app/src/main/java/com/example/temusic/Ui/Activitys/MainActivity.kt
@@ -1,30 +1,61 @@
 package com.example.temusic.Ui.Activitys
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentManager
 import com.example.temusic.R
 import com.example.temusic.Ui.ui.fragments.AlbumsFragment
 import com.example.temusic.Ui.ui.fragments.MusicFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var bottomnavigation:BottomNavigationView? = null
     var manager:FragmentManager = supportFragmentManager
+lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         showalbumFragment()
         bottomnavigation = findViewById(R.id.btn_nav)
-        bottomnavigation!!.setOnNavigationItemSelectedListener{
-            when(it.itemId){
-                R.id.nav_music->showmusicFragment()
-                R.id.nav_allbom->showalbumFragment()
+        bottomnavigation!!.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_music -> showmusicFragment()
+                R.id.nav_allbom -> showalbumFragment()
             }
             return@setOnNavigationItemSelectedListener true
         }
+        ///////////////////////////////drawer
+        setSupportActionBar(findViewById(R.id.toolbar_n))
+        toggle= ActionBarDrawerToggle(this, drawer_layout ,R.string.drawer_open,R.string.drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        nav_view.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.na_share -> Toast.makeText(applicationContext, "share", Toast.LENGTH_SHORT).show()
+                R.id.na_rate -> Toast.makeText(applicationContext, "rate", Toast.LENGTH_SHORT).show()
+                R.id.na_aboutus -> Toast.makeText(applicationContext, "about us", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+        {return true}
+        return super.onOptionsItemSelected(item)
+    }
+    ////////////////////////////
      private fun showmusicFragment(){
         val transactin = manager.beginTransaction()
         val fragment = MusicFragment()
